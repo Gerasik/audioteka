c299c4a96697db041e291376061cb0bcf148649f
Gerasik <gerasik1992@gmail.com>
2020-02-11 22:34:24 +0300

    edit ignore file

---
+++ precache-manifest.c18fed66e7c85e582a63229f3d21ba0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d607d73ae5128b82a57ec6c265c04a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "551ff0e564438c981ff0",
+    "url": "/static/css/main.a2c4c786.chunk.css"
+  },
+  {
+    "revision": "10e566cb00e77448b42c",
+    "url": "/static/js/2.52593c28.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.52593c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551ff0e564438c981ff0",
+    "url": "/static/js/main.53ec51aa.chunk.js"
+  },
+  {
+    "revision": "b96ecea70c828c40687f",
+    "url": "/static/js/runtime-main.d5963870.js"
+  }
+]);
