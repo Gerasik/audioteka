0b4c6380c4b43059340c92f34031a8053e1178b1
Gerasik <gerasik1992@gmail.com>
2020-02-11 23:22:18 +0300

    build test

---
+++ precache-manifest.25a70be926025f6a36bfa4ae23582fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2369d3f40cdc7289224ca69c3507ce",
+    "url": "/audioteka/index.html"
+  },
+  {
+    "revision": "de7c55d8cf738232fc2e",
+    "url": "/audioteka/static/css/main.a2c4c786.chunk.css"
+  },
+  {
+    "revision": "10e566cb00e77448b42c",
+    "url": "/audioteka/static/js/2.52593c28.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/audioteka/static/js/2.52593c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7c55d8cf738232fc2e",
+    "url": "/audioteka/static/js/main.79c49f59.chunk.js"
+  },
+  {
+    "revision": "f579acc3159b4080c442",
+    "url": "/audioteka/static/js/runtime-main.941cb39a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c18fed66e7c85e582a63229f3d21ba0a.js"
+  "/audioteka/precache-manifest.25a70be926025f6a36bfa4ae23582fa9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/audioteka/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
