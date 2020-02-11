e2e91800f26530389014245e670dd7a0a535c985
Gerasik <gerasik1992@gmail.com>
2020-02-11 15:51:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import soundList from './data/csvjson.json'
 
 function App() {
+  function getUnique(arr, attr) {
+    let result = [];
+    for (let str of arr) {
+      if (!result.includes(str[attr])) {
+        result.push(str[attr]);
+      }
+    }
+    return result.sort();
+  }
+  const singers = getUnique(soundList, 'singer');
+  const ganres = getUnique(soundList, 'ganre');
+  const years = getUnique(soundList, 'year');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        {
+          soundList.map((item)=>
+            <tr key={item.id}>
+              <td>{item.singer}</td>
+              <td>{item.song}</td>
+              <td>{item.ganre}</td>
+              <td>{item.year}</td>
+            </tr>
+          )
+        }
+      </table>
+      <ul>
+        {singers.map(item=> <li>{item}</li>)}
+      </ul>
+      <ul>
+        {ganres.map(item=> <li>{item}</li>)}
+      </ul>
+      <ul>
+        {years.map(item=> <li>{item}</li>)}
+      </ul>
+      {/* <select onChange={this.handleChange}> */}
+      <select>
+        <option>All</option> 
+        {singers.map(item=> <option>{item}</option>)}
+      </select>
     </div>
   );
 }
